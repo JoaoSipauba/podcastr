3262b130dc20678db1a50ceaf5e85fe3ec2c3a61
joao.sipauba <joao.sipauba@ceuma.br>
2021-04-20 17:44:08 -0300

    feat: creating player component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import Head from 'next/head'
-import { Header } from '../components/Header'
-// import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <>
-      <Header />
-      <h1>Oi</h1>
-    </>
+    <h1>Index</h1>
   )
 }
