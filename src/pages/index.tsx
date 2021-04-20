59e2545a5708b4a4f3aeb8458fa5281327c8ed92
joao.sipauba <joao.sipauba@ceuma.br>
2021-04-20 18:21:18 -0300

    feat: consuming the fake API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,16 @@ export default function Home() {
     <h1>Index</h1>
   )
 }
+
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
